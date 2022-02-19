2961905377c86355acfdf173691d1a5887a260e9
Yashar Hassanpour <yasharh@miltenyibiotec.de>
2022-02-19 18:02:23 +0100

    final

---
+++ whatsnewindotnet6/whatsnewindotnet6/Foo.cs
@@ -0,0 +1,9 @@
+
+
+// Cannot use List if using i not global in other file
+
+
+public class Foo
+{
+    public List<string> someList { get; set; }
+}
--- whatsnewindotnet6/whatsnewindotnet6/WhatsNewInDotnet.cs
+++ whatsnewindotnet6/whatsnewindotnet6/WhatsNewInDotnet.cs
@@ -1,7 +1,18 @@
-﻿using System;
+﻿
+
+
+
+// Global using allow to declare a using in a file and use it in all code
+
+
+
+
+global using System.Collections.Generic;
+// using System.Collections.Generic;
 
 class WhatsNewInDotnet
 {
+    public List<int> someList { get; set; }
     static void Main()
     {
     }
