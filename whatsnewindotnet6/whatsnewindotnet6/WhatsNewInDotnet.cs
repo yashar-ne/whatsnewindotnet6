497fb285e0fe583cd68e8590d8d32db3eb6b5539
Yashar Hassanpour <yasharh@miltenyibiotec.de>
2022-02-19 18:10:29 +0100

    final

--- whatsnewindotnet6/whatsnewindotnet6/WhatsNewInDotnet.cs
+++ whatsnewindotnet6/whatsnewindotnet6/WhatsNewInDotnet.cs
@@ -1,8 +1,27 @@
-﻿using System;
+﻿
 
-class WhatsNewInDotnet
+
+
+// C#10 allows namespaces for the whole file by not putting it in curly braces like:
+
+namespace SomeNormalNamespace
 {
-    static void Main()
+    class WhatsNewInDotnet
     {
+        static void Main()
+        {
+        }
     }
 }
+
+
+// Now you can do for the whole file:
+//
+// namespace SomeFileScopedNamespace;
+//
+// class WhatsNewInDotnet
+// {
+//     static void Main()
+//     {
+//     }
+// }
