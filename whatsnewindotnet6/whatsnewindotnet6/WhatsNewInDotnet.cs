e4af7a1e263b5e596b5346b4698f7443e4bdaaeb
Yashar Hassanpour <yasharh@miltenyibiotec.de>
2022-02-20 17:00:24 +0100

    final

--- whatsnewindotnet6/whatsnewindotnet6/WhatsNewInDotnet.cs
+++ whatsnewindotnet6/whatsnewindotnet6/WhatsNewInDotnet.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Threading;
 
 class WhatsNewInDotnet
 {
 
     static void Main()
     {
+
+        while (true)
+        {
+            Print();
+        }
+    }
+
+    private static void Print()
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Some nice output");
     }
 }
