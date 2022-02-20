ea75eb250f834874b15344130a3e56f410405983
Yashar Hassanpour <yasharh@miltenyibiotec.de>
2022-02-20 16:28:40 +0100

    final

--- whatsnewindotnet6/whatsnewindotnet6/WhatsNewInDotnet.cs
+++ whatsnewindotnet6/whatsnewindotnet6/WhatsNewInDotnet.cs
@@ -5,5 +5,22 @@ class WhatsNewInDotnet
 
     static void Main()
     {
+        // Before C#10 automatic inference of the datatype in lambda expressions was not possible
+        Func<string, bool> isItTrue = someInput => someInput == "foo";
+        
+        //Now it is!
+        var isItReallyTrue = (string someInput) => someInput == "foo";
+
+        
+        
+        
+        
+        
+        
+        
+        //Also the return type of a lambda can be set explicitly
+        var someValue = () => 1;        
+        var someOtherValue = () => (long) 1;
+
     }
 }
